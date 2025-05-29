c71b909059e5119931fb84680bf4a0ee63bfe0ae
Aman Kumar <138227834+thisisaman408@users.noreply.github.com>
2025-05-29 15:52:56 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { FaGithub, FaInstagram } from 'react-icons/fa';
 const links = [
 	{
-		href: `https://www.github.com/thisisaman408`,
+		href: `https://www.github.com`,
 		icon: <FaGithub />,
 	},
 	{
-		href: `https://www.instagram.com/thisisaman408`,
+		href: `https://www.instagram.com/nutassa`,
 		icon: <FaInstagram />,
 	},
 ];
@@ -15,7 +15,7 @@ const Footer = () => {
 		<footer className="w-screen bg-violet-300 py-4 text-black">
 			<div className="container mx-auto flex flex-col  justify-between items-center gap-4 px-4 md:flex-row">
 				<p className="text-center text-sm md:text-left">
-					©Aman 2025. All Rights Reserved
+					©Natasha 2025. All Rights Reserved
 				</p>
 				<div className="flex justify-center gap-4  md:justify-start">
 					{links.map((link, index) => (
